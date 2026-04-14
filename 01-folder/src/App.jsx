560f971c77707910fe5fba816ac602a2b6a98c7c
Anshul Gour <anshulgaur7999@gmail.com>
2026-04-14 16:07:58 +0530

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div>
+      <h1>hello anshul gour</h1>
+      <h2>this is your react website</h2>
+    </div>
+  )
+}
+export default App
