72ea41aaaed2cd9402f49dfad99e9132b8732527
Anshul Gour <anshulgaur7999@gmail.com>
2026-04-16 18:29:15 +0530

    02-componets

---
+++ 02-component/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ const Navbar = () => {
+  return (
+
+  <div className="nav">
+    navbarr hi khende </div>
+       
+  ) 
+}
+export default Navbar 
---
+++ 02-component/src/components/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div>
+            <h1>Anshul Gour</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Beatae earum quaerat facilis quas fugiat, tempora qui tempore eum pariatur in dicta
+                id dignissimos numquam officiis ullam at perferendis, nisi laboriosam!</p>
+        </div>
+    )
+}
+export default Card
+
